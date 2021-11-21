3e5ee2f1510719eb72593d13dacfec60458a7cf4
siriavula <sirigavula@gmail.com>
2021-11-21 13:15:06 -0500

    adding comments

--- state/State.java
+++ state/State.java
@@ -1,6 +1,28 @@
+/**
+ * This is the interface for State and it has four methods.
+ * 
+ * @author Siri Avula
+ */
 public interface State {
+    /**
+     * This method gets us a random number in a certain range depending on the state level.
+     * @return a number that will be used in asking the user a Arithmetic question for the Arithemtic game.
+     */
     public int getNum();
+
+    /**
+     * This method gives us a random operation depending on the state level.
+     * @return returns an operation as a String
+     */
     public String getOperation();
+
+    /**
+     * This method levels up to the next state depending on the current state.
+     */
     public void levelUp();
+
+    /**
+     * This method levels down to the previous state depending on the current state.
+     */
     public void levelDown();
 }
